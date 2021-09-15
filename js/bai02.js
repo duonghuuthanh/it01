3911cf0d2cd31084cec5c51c4dcddd19a7e6d5ee
Thanh Duong <dhthanhqa@gmail.com>
2021-09-15 16:18:45 +0700

    update chieu

--- js/bai02.js
+++ js/bai02.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+    // new WOW().init();
+
+    $("div.figures > figure").addClass("wow animate__slideInUp")
+    $("div.items > div.item:nth-child(even)").addClass("wow animate__slideInLeft")
+    $("div.items > div.item:nth-child(odd)").addClass("wow animate__slideInRight")
+    wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animate__animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+      }
+      )
+      wow.init();
     $("#btnThem").click(function() {
         var content = $("#content").val()
         var date = $("#date").val()
@@ -21,5 +36,10 @@ $(document).ready(function() {
         setTimeout(function() {
             $(".item").removeClass("active")
         }, 2000);
+    })
+
+    $("div.items").on("click", "div.item input[type=button]", function() {
+        if (confirm("Ban co chac chan xoa cong viec?") == true)
+            $(this).parent().parent().remove()
     })
 })
